b6f3009cc43f8ca8a4995af9c667e61842cf0685
陈爱彬 <136998328@qq.com>
2017-01-16 16:43:18 +0800

    update

--- demo/TXCycleScrollView/ViewController.m
+++ demo/TXCycleScrollView/ViewController.m
@@ -21,21 +21,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    [self setupBannerData];
     [self setupBannerView];
 }
 
-- (void)setupBannerData
-{
-    self.bannerList = @[
-                        @"http://dynamic-image.yesky.com/740x-/uploadImages/2014/188/15/4EABKK639PIS.jpg",
-                        @"http://img1.3lian.com/2015/a2/239/d/289.jpg",
-                        @"http://img.hb.aicdn.com/66ec45fe337d20fdb5050aaf81048b8975a518aa1a0f7-J9kgSC_fw580",
-                        @"http://img0.ph.126.net/Y3AM_Lxfz4fonBDQpNlkSQ==/6597234693400985046.jpg"
-                        ];
-}
 - (void)setupBannerView
 {
+    self.bannerList = @[
+                    @"http://dynamic-image.yesky.com/740x-/uploadImages/2014/188/15/4EABKK639PIS.jpg",
+                    @"http://img1.3lian.com/2015/a2/239/d/289.jpg",
+                    @"http://img.hb.aicdn.com/66ec45fe337d20fdb5050aaf81048b8975a518aa1a0f7-J9kgSC_fw580",
+                    @"http://img0.ph.126.net/Y3AM_Lxfz4fonBDQpNlkSQ==/6597234693400985046.jpg"
+                    ];
+
     TXCycleScrollView *bannerView = [[TXCycleScrollView alloc] initWithFrame:CGRectMake(0, 50, self.view.bounds.size.width, 200)];
     bannerView.delegate = self;
     bannerView.imageUrls = self.bannerList;
